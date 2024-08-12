aa7eabaf213e0b78929ec868b082eca170ecfe66
Son Rodrey Inocencio <Rodrey@DESKTOP-AN6EU0S>
2024-08-12 12:52:24 +1200

    made the controller ftoc

---
+++ FtoC/Controllers/FtoC.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FtoC.Controllers
+{
+    public class FtoC : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public String ftoc(double f)
+        {
+            double c = (f - 32) * 100 / 180;
+
+            string result = "The converted Fareinheit " + f + " is equiavalent to " + c + " degree celcius.";
+            return result;
+        }
+
+
+    }
+}
